8241204c6d7886dd27643fdba374a561daa62d5f
Jimbo145 <cpickering145@gmail.com>
2023-04-10 15:50:24 -0400

    Dev (#16)
    * Change Service script location
    
    * Remove gitpython
    
    * Attempt to move Fog light on and aux change to updater.
    
    * Change Updater to run in a standalone service
    
    * Change Updater to run in a standalone service
    
    * Modify service files so they point to right path and are executed in order
    
    * modify install.sh to work if already installed. Move After key in saab.service
    
    * modify radio change messages, move foglight on back to saabCan.py
    
    * Change copy command to bash
    
    * remove attempt at automatic radio change. change back to get pull
    
    * add api (currently commented out).
    add journalctl logging
    add automatic source changing
    
    * Add Async to get_battery_status and extend install.sh
    
    * add requirements.txt
    change systemd journal handling
    
    ---------
    
    Co-authored-by: CPickeringBR <cole.pickering@br-automation.com>
    Co-authored-by: cole <cole@colepickering.com>

--- saab-control/saabCan.py
+++ saab-control/saabCan.py
@@ -11,12 +11,16 @@ import sys
 import time
 import shutil
 import os
-import systemd.journal
+from systemd.journal import JournalHandler # pip install systemd
+
+# import sqlite3
+# from aiohttp import web
 # import sqlite3          #pip install sqlite3
 # from aiohttp import web #pip install aiohttp
     #pip install pyzmq
 
 keyboard = Controller()
+
 
 messageStore = {}
 LogChange: bool = True
@@ -234,7 +238,8 @@ async def send_message(bus: can.Bus,can_id: str, data:bytearray):
 async def get_battery_status():
     run = subprocess.run(['lifepo4wered-cli' , 'get', 'vbat'], check=True, text=True)
     battery_voltage = float(output.decode().strip())
-    systemd.journal.send("Battery voltage: {:.2f} V".format(battery_voltage))
+
+    logging.info("Battery voltage: {:.2f} V".format(battery_voltage))
     await asyncio.sleep(10)
     asyncio.create_task(get_battery_status())
 
@@ -360,6 +365,9 @@ async def main(test_mode) -> None:
 try:
     if __name__ == "__main__":
         print("Starting...")
+        log = logging.getLogger('demo')
+        log.addHandler(JournalHandler())
+        log.setLevel(logging.INFO)
         logging.info("Starting")
         if os.path.exists('/usr/local/bin/SaabHeadUnitUpdater/update'):
             subprocess.call(['sudo', 'cp', '/usr/local/bin/SaabHeadUnit/saabUpdate.py',
